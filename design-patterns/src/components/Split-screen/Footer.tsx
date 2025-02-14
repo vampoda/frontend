9d7f809aabe9c9a795b58a9a4843995c04f2266a
Vamsi A <vampvamsi@gmail.com>
2025-02-14 21:28:04 +0530

    split screen pattern

---
+++ design-patterns/src/components/Split-screen/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <footer className="bg-teal-800 text-white p-4 text-center">
+        <p>&copy; 2025 My Dashboard App</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
